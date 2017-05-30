7fc74a0f0d552e77434ab8341ed4484236af6178
meartemaa <artemaa@gmail.com>
2017-05-30 11:47:56 +0300

    Added posgress + tomcat polling. Finished Restaurant data jpa tests

--- src/test/java/ru/artemaa/topjavagraduate/dao/datajpa/DataJpaRestaurantDaoTest.java
+++ src/test/java/ru/artemaa/topjavagraduate/dao/datajpa/DataJpaRestaurantDaoTest.java
@@ -1,5 +1,7 @@
 package ru.artemaa.topjavagraduate.dao.datajpa;
 
+import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import ru.artemaa.topjavagraduate.dao.RestaurantDao;
 import ru.artemaa.topjavagraduate.model.Restaurant;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import static ru.artemaa.topjavagraduate.RestaurantTestData.*;
 
@@ -20,7 +23,7 @@ import static ru.artemaa.topjavagraduate.RestaurantTestData.*;
  */
 @ContextConfiguration({
         "classpath:spring/spring-app.xml",
-        "classpath:spring/spring-db-test.xml"
+        "classpath:spring/spring-db.xml"
 })
 @RunWith(SpringJUnit4ClassRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
@@ -38,14 +41,29 @@ public class DataJpaRestaurantDaoTest {
 
     @Test
     public void testGet() throws Exception {
+        Restaurant restaurant = dao.get(REST1_ID);
+        MATCHER.assertEquals(REST1, restaurant);
+    }
+
+    @Test
+    public void testGetNotFound() throws Exception {
+        Assert.assertNull(dao.get(1));
     }
 
     @Test
     public void testDelete() throws Exception {
+        dao.delete(REST1_ID);
+        MATCHER.assertCollectionEquals(Collections.singletonList(REST2), dao.getAll());
+    }
+
+    @Test
+    public void testDeleteNotFound() throws Exception {
+        Assume.assumeFalse(dao.delete(1));
     }
 
     @Test
     public void testGetAll() throws Exception {
+        MATCHER.assertCollectionEquals(Arrays.asList(REST1, REST2), dao.getAll());
     }
 
 }
